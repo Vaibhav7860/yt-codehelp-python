1777ffc17e4a8c24517eafaad05c511a1c9f80af
Abhinav Dhiman <abhinavg916@gmail.com>
2020-05-20 06:38:32 +0530

    Moved to respective folders

--- Assignments/Hackerrank/Division.py
+++
@@ -1,6 +0,0 @@
-a = int(input())
-b = int(input())
-c = int(a/b)
-d = float(a/b)
-print(c)
-print(d)
--- Assignments/Hackerrank/PrintFunctoin.py
+++
@@ -1,5 +0,0 @@
-n = int(input())
-def printNumbers(x):
-    print(x+1, end= "")
-for x in range(n):
-    printNumbers(x)
